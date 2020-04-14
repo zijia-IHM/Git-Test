3573a54ccdbb478e023b139bb500028b5acf8aaa
ZijiaZhai <zijiazhai@iheart.com>
2020-04-14 10:22:18 -0500

    first commit

---
+++ Git-Test/Git-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git-Test
+//
+//  Created by Zijia Zhai on 4/14/20.
+//  Copyright © 2020 Zijia Zhai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
