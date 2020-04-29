352d1e60ce39e29870d43a5626b8d7f0845168a8
ZijiaZhai <zijiazhai@iheart.com>
2020-04-29 16:20:34 -0500

    Revert "test3"
    This reverts commit 3f8da0e4534853c974798784f0236760ba42ae2a.

--- Git-Test/Git-Test/ViewController.swift
+++ Git-Test/Git-Test/ViewController.swift
@@ -21,14 +21,10 @@ class ViewController: UIViewController {
 
         viewcontrollerDelegate?.didSelected()
 
-
-        print("test3")
-
         print("1231231")
-        print("test3")
         print("1231231")
 
-        print("test3")
+        print("test1")
     }
 
 
