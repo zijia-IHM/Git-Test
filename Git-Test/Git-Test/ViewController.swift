f2d73ba09a34a0817c9d114bbbb94c9dd028f5a5
ZijiaZhai <zijiazhai@iheart.com>
2020-11-13 14:18:30 -0600

    remove all data

--- Git-Test/Git-Test/ViewController.swift
+++ Git-Test/Git-Test/ViewController.swift
@@ -19,24 +19,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         viewcontrollerDelegate?.didSelected()
-
-
-        print("t11")
-        print("t22")
-        print("t1")
-        print("t2")
-        print("test2")
-        print("test3")
-        print("test1")
-        print("test11")
-        print("test111")
-        print("test2")
-        print("t1")
-        print("t2")
-        print("t11")
-        print("t22")
-        print("resthard 1")
-        print("resthard 2")
     }
 
 
